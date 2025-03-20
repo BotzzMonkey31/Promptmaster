2fda833c610c9d6a2ee81de191940734b8283d0f
Bataire Enrique <145664630+BotzzMonkey31@users.noreply.github.com>
2025-03-20 13:54:46 +0100

    Ref: fix merge error

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,12 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-<<<<<<< Updated upstream
-import HomeView from '../views/HomeView.vue'
-=======
-import HomeView from '../views/Home.vue'
+import HomeView from '@/views/Home.vue'
 import PrivacyPolicy from '@/views/PrivacyPolicy.vue'
 import TermsOfService from '@/views/TermsOfService.vue'
 import PuzzleOverview from '@/views/PuzzleOverview.vue'
->>>>>>> Stashed changes
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,12 +13,14 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/privacy',
+      name: 'privacy',
+      component: PrivacyPolicy,
+    },
+    {
+      path: '/terms',
+      name: 'terms',
+      component: TermsOfService,
     },
     {
       path: '/puzzle',
